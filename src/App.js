5ead0cd1f90d18280777151dff0b1d3628c8c330
emmaxtung <emmaxtung@gmail.com>
2022-10-15 16:57:27 -0500

    add react calendar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import logo from './logo.svg';
+import { DateRange } from "react-date-range";
+import {useState} from 'react'
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import 'react-date-range/dist/styles.css'; 
+import 'react-date-range/dist/theme/default.css';
 import './App.css';
 
+
 function App() {
+  const [state, setState] = useState([
+    {
+      startDate: new Date(),
+      endDate: null,
+      key: "selection"
+    }
+  ]);
+  
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          HackTX 2022 Project
-        </p>
+        <div>
+          <h2>
+            Weather to Pack or Not?
+          </h2>
+          <h4>
+            Choose your trip date range:
+          </h4>
+          <DateRange
+            onChange={item => setState([item.selection])}
+            moveRangeOnFirstSelection={false}
+            ranges={state}
+          />
+          <form>
+            Input city name:
+            <TextField id="standard-basic" label="Standard" variant="standard" />
+            <Button variant="contained">Submit</Button>
+            <br/>
+          </form>
+        </div>
       </header>
     </div>
   );
